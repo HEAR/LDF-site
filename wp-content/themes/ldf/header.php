cf4a9f4740bf176764e1c7599dd0a65e5fbac661
Loïc Horellou <loic@formidable-studio.net>
2014-05-19 22:18:51 +0200

    update header.php
    + modification du sélecteur de langue pour le lier à WPML
    + ajout du logo HD sous forme de balise img
    + correction du lien home sur le logo (récupération de la langue)
    + update du symbole pour déplier le menu thématique
    + ajout d’un bloc autour du slogan + description (utile pour css print)

--- wp-content/themes/ldf/header.php
+++ wp-content/themes/ldf/header.php
@@ -35,27 +35,49 @@
 	<div class="right-box clr fs13 clr-bc cgb">
 		<div class="container">
 			<?php get_search_form(); ?>
-			<a class="up fr lang" href="<?php echo esc_url( home_url( '/?lang=fr' ) ); ?>">fr</a>
-			<!-- <a class="up en lang" href="<?php echo esc_url( home_url( '/?lang=en' ) ); ?>">en</a> !-->
-			<a class="up de lang" href="<?php echo esc_url( home_url( '/?lang=de' ) ); ?>">de</a> <span class="lang_select"></span><a href="https://twitter.com/LDF_1914_2018" class="fa fa-twitter twitter"></a> <a href="https://www.facebook.com/lignesdefront.1914.2018" class="fa fa-facebook facebook"></a>
-			<?php //do_action('icl_language_selector'); ?>
+			<span class='langselector'>
+				<?php
+					languages_list();
+					function languages_list(){
+					    $languages = icl_get_languages('skip_missing=1'); // &orderby=code
+					    if(!empty($languages)){
+					    	//$i = 0;
+					        foreach($languages as $l){
+					        	//echo $i>0 ? ' / ' : '';
+					            echo ' <span class="lang up">';
+					            if(!$l['active']) echo '<a href="'.$l['url'].'">';
+					            echo substr($l['native_name'], 0,2 );
+					            if(!$l['active']) echo '</a>';
+					            echo '</span>';
+					            //$i++;
+					        }
+					    }
+					}
+				?>
+				<span class="selector">></span>
+			</span>
+
+			<a href="https://twitter.com/LDF_1914_2018" class="fa fa-twitter twitter"></a>
+			<a href="https://www.facebook.com/lignesdefront.1914.2018" class="fa fa-facebook facebook"></a>
 		</div>
 	</div>
 	<div class="left-box cgb up">
 		<div class="head-box ">
 		
 			<h1 class="site-title">
-				<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?>
+				<a class="home-link" href="<?php echo icl_get_home_url(); //bloginfo('url'); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo-HD.png" width="112" height="112" alt="<?php bloginfo( 'name' ); ?>" title="<?php bloginfo( 'name' ); ?>" id="logo" />
 				</a>
 			</h1>
-		
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			<h2 class="site-sub-description"><?php _e( 'Programme de recherche', 'twentythirteen' );  ?></h2>
+			<div class="slogan">
+				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				<h2 class="site-sub-description"><?php _e( 'Programme de recherche', 'twentythirteen' );  ?></h2>
+			</div>
 		</div>
 		<div class="nav-box">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 			
-			<h3><a class="fs13 thema-toggle" ><?php _e( 'Acces thematique', 'twentythirteen' ); ?></a></h3>
+			<h3><a class="fs13 thema-toggle" ><?php _e( 'Acces thematique', 'twentythirteen' ); ?><span class="toggle">&or;</span></a></h3>
 			
 			<?php wp_nav_menu( array( 'theme_location' => 'themas', 'menu_class' => 'nav-toggle nav-menu' ) ); ?>
 		</div>
